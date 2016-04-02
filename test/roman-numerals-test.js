73ad939ea915210078261a27a63fc95aab0f9aab
Vaidas Pilkauskas <vaidasp@wix.com>
2016-04-02 10:35:15 +0300

    Add solution with mutable state

--- test/roman-numerals-test.js
+++ test/roman-numerals-test.js
@@ -1,11 +1,53 @@
+'use strict'
 const expect = require('chai').expect;
 
 describe('Roman Numerals Converter', () => {
-  it('converts 1 to I', () => {
-    expect(toRoman(1)).to.equal('I');
+  const tests = [
+    [1, 'I'],
+    [2, 'II'],
+    [3, 'III'],
+    [4, 'IV'],
+    [5, 'V'],
+    [6, 'VI'],
+    [9, 'IX'],
+    [10, 'X'],
+    [16, 'XVI'],
+    [20, 'XX'],
+    [40, 'XL'],
+    [50, 'L'],
+    [90, 'XC'],
+    [100, 'C'],
+    [400, 'CD'],
+    [500, 'D'],
+    [900, 'CM'],
+    [1000, 'M']
+  ];
+  
+  tests.forEach(test => {
+    const arabic = test[0];
+    const roman = test[1];
+
+    it(`converts ${arabic} to ${roman}`, () => {
+      expect(toRoman(arabic)).to.equal(roman);
+    });
+ 
+
   });
+
 });
 
-function toRoman() {
-  return 'I';
+const RULES = {
+  M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 
+};
+
+function toRoman(number) {
+  function applyRule(roman, arabic) {
+    const times = Math.floor(number / arabic);
+    number -= arabic * times; 
+    return roman.repeat(times);
+  }
+
+  return Object.keys(RULES).map(roman => applyRule(roman, RULES[roman])).join('');
 }
+
+
