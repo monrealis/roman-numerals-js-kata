85e43b53a7396970b2dcdf7b2248e9fc70f76658
Vytenis Bivainis <monrealis@gmail.com>
2016-05-22 15:17:40 +0300

    Try to call mocha from phantom.js.

--- phantom.js
+++ phantom.js
@@ -3,7 +3,18 @@ page.open('mocha.html', function(status) {
 	console.log("Status: " + status);
 	if (status === "success") {
 		setTimeout(function() {
-			page.render('example.png');
+			var title = page.evaluate(function() {
+				return document.title;
+			});
+			var passed = page.evaluate(function() {
+				return document.querySelector('.passes').innerText;
+			});
+			var failed = page.evaluate(function() {
+				return document.querySelector('.failures').innerText;
+			});
+			console.log('Page title is ' + title);
+			console.log(passed + ' / ' + failed);
+			// page.render('mocha.png');
 			phantom.exit();
 		}, 1000);
 	} else {
