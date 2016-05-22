7a804d4e9bda128ef2481ae1702f903220fd8e1b
Vytenis Bivainis <monrealis@gmail.com>
2016-05-23 00:02:58 +0300

    Implement polling and exit status.

--- phantom.js
+++ phantom.js
@@ -8,22 +8,35 @@ page.onConsoleMessage = function(msg, lineNum, sourceId) {
 page.open('mocha.html', function(status) {
 	console.log("Status: " + status);
 	if (status === "success") {
-		setTimeout(function() {
-			var title = page.evaluate(function() {
-				return document.title;
-			});
-			var passed = page.evaluate(function() {
-				return document.querySelector('.passes').innerText;
-			});
-			var failed = page.evaluate(function() {
-				return document.querySelector('.failures').innerText;
-			});
-			console.log('Page title is ' + title);
-			console.log(passed + ' / ' + failed);
-			// page.render('mocha.png');
-			phantom.exit();
-		}, 1000);
+		setInterval(checkIfFinished, 200);
 	} else {
 		phantom.exit();
 	}
 });
+
+var checkIfFinished = function() {
+	var title = page.evaluate(function() {
+		return document.title;
+	});
+	var passed = page.evaluate(function() {
+		return document.querySelector('.passes').innerText;
+	});
+	var failed = page.evaluate(function() {
+		return document.querySelector('.failures').innerText;
+	});
+	var finished = page.evaluate(function() {
+		return mochaRunner.stats.tests == mochaRunner.total;
+	});
+	var failedFromStats = page.evaluate(function() {
+		return mochaRunner.stats.failures;
+	});
+	console.log(passed + ' / ' + failed);
+	if (finished) {
+		console.log(failedFromStats);
+		page.evaluate(function() {
+			document.body.bgColor = 'white';
+		});
+		page.render('mocha.png');
+		phantom.exit(failedFromStats);
+	}
+}
